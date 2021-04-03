554f27565d2e5712b2d1561536b4f7e531aa61e8
MayankArya123 <mayanksunny786@gmail.com>
2021-04-03 11:11:40 +0530

    initial commit

---
+++ login/src/App.js
@@ -0,0 +1,31 @@
+//import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App"> 
+      <div className="first_half_container">
+      <div className="form_container">
+         <div className="logo_image">
+             <span>spanjjjjjjjhhjjjj</span>
+         </div>
+      <p className="paragraph"> Type in your password</p>
+          <div className="form_group">
+            <input placeholder="username" type="text"/>
+          </div>
+          <div className="form_group">
+          <input placeholder="password" type="text" />
+        </div>
+        <button className="login_button">login</button>
+      </div>
+      
+      </div>
+    
+       <div className="second_half_container">
+         
+       </div>
+    </div>
+  );
+}
+
+export default App;
